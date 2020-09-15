6e3d784467bdec3b649f907a48ee32a8591cd68a
Ajay Yamkar <58873896+Ajyamkar@users.noreply.github.com>
2020-09-15 20:48:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function validation(){
+  var x,text;
+   x=document.querySelector(".input1").value;
+  if(x.search('@'+"")==-1){
+    text="Please provide a valid email";
+    var answer=document.getElementById("answer");
+    answer.innerHTML = text;
+    answer.style.position="relative";
+    answer.style.bottom="43px";
+    answer.style.left="7px";
+    answer.style.fontSize="12px";
+    answer.style.color="hsl(0, 93%, 68%)";
+    return false;
+  }
+
+}
